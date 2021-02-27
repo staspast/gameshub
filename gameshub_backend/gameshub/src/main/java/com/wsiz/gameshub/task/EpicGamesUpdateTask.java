86632457937d0cf608aae8cab9d8782c77341813
Vladyslav Pastushenko <vladpastu@gmail.com>
2021-02-27 17:59:56 +0100

    epic games scheduled update tast

---
+++ gameshub_backend/gameshub/src/main/java/com/wsiz/gameshub/task/EpicGamesUpdateTask.java
@@ -0,0 +1,34 @@
+package com.wsiz.gameshub.task;
+
+import com.wsiz.gameshub.constant.MarketPlaceConstants;
+import com.wsiz.gameshub.decorator.EpicGamesDecorator;
+import com.wsiz.gameshub.model.entity.Game;
+import com.wsiz.gameshub.model.repository.GamesRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class EpicGamesUpdateTask implements UpdateTask{
+
+    private final GamesRepository gamesRepository;
+    private final EpicGamesDecorator decorator;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Scheduled(fixedDelay = 10000)
+    public void updateNonUpdatedGameData(){
+        List<Game> gamesToProcess = gamesRepository.findNonLoadedByMarketplaceName(MarketPlaceConstants.MARKETPLACE_NAME_EPIC_GAMES, PageRequest.of(0, 10));
+
+        log.info("Epic games to process {}", gamesToProcess.size());
+
+        gamesToProcess.forEach(decorator::decorate);
+    }
+}
